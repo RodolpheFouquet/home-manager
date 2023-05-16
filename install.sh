ba4533787d8ff73714de7e79cde2dbd6f1fea8d3
Rodolphe Fouquet <rodolphe@unicowd.com>
2023-05-16 15:10:05 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm -rf $HOME/.config/home-manager/*
+cp -r src/home-manager/* $HOME/.config/home-manager/ 
--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-
 rm -rf src/*
 cp -r $HOME/.config/home-manager/ src/home-manager
