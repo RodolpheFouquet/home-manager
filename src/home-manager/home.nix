a9c2ec0b0e051e513c05de14fb793b5f148252e6
Rodolphe Fouquet <rodolphe@unicowd.com>
2023-05-21 04:00:56 +0100

    update optiosn and default browser

--- src/home-manager/home.nix
+++ src/home-manager/home.nix
@@ -131,7 +131,7 @@
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
     EDITOR = "nvim";
-    BROWSER = "firefox";
+    BROWSER = "google-chrome-stable";
     TERMINAL = "kitty";
     TERM = "kitty";
     VISUAL = "nvim"; 
