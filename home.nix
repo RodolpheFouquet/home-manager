816e3fc8dbebcacce1021cf76abc06e879425f14
Rodolphe Fouquet <rodolphe@unicowd.com>
2024-04-27 23:55:18 +0100

    start v1 work

--- home.nix
+++ home.nix
@@ -1,25 +1,31 @@
 { config, pkgs, ... }:
-let
-  nixvim = import (builtins.fetchGit {
-    url = "https://github.com/nix-community/nixvim";
-  });
-in
+
 {
-
-  imports = [
-    nixvim.homeManagerModules.nixvim
-  ];
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "rodolphe";
-  home.homeDirectory = "/home/rodolphe";
-
   nixpkgs = {
     config = {
       allowUnfree = true;
       allowUnfreePredicate = (_: true);
     };
   };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+
+  
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "vachicorne";
+  home.homeDirectory = "/home/vachicorne";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
@@ -28,212 +34,81 @@ in
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    btop
-    feh
-    gh
-    steam
-    ripgrep
-    autorandr
-    discord
-    flashprint
-    pavucontrol
-    rofi
-    firefox
-    skypeforlinux
-    vmware-horizon-client
-    spotify-player
-    zoom-us
-    curl
-    ffmpeg
-    unzip
-    ranger
-    viewnior
-    killall 
-    wget
-    zip
-    e2fsprogs
-    google-chrome
-    (pkgs.polybar.override {
-      i3Support = true;
-      alsaSupport = true;
-      iwSupport = true;
-      githubSupport = true;
-    })
-    zellij
-    picom
-    dunst
-    lxappearance
-    gtk_engines
-    xclip
-    bat
-    fd
-    (pkgs.nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "JetBrainsMono"]; })
+      firefox
+      steam
+      skypeforlinux
+      discord
+      nvtopPackages.nvidia
+      mangohud
+      obs-studio
+      sapling
+      gh
+      git
+      git-lfs
+      libratbag
+      piper
+      mesa-demos
+      zoom-us
+      vmware-horizon-client
+      usbutils
+      ffmpeg
+      vlc
+      mpv
+      wezterm
+      starship
+      unzip
+      ripgrep
+      gpu-screen-recorder-gtk
+      google-chrome
+      zellij
+      teamspeak5_client
+      bat
+      plank
+
+      rustup
+      nodejs
+      clang
+      zig
+      gleam
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".config/i3".source = dotfiles/i3;
-    ".config/i3".recursive = true;
-    ".config/picom".source = dotfiles/picom;
-    ".config/picom".recursive = true;
-    ".config/zellij".source = dotfiles/zellij;
-    ".config/zellij".recursive = true;
-    ".config/polybar".source = dotfiles/polybar;
-    ".config/polybar".recursive = true;
-    ".config/dunst".source = dotfiles/dunst;
-    ".config/dunst".recursive = true;
-    ".config/wallpaper.png".source = dotfiles/wallpaper.png;
+    ".config/wezterm/wezterm.lua".source = config/wezterm/wezterm.lua;
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
   };
 
-  programs.nixvim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-
-    globals = {
-      mapleader = " ";
-    };
-
-    options = {
-      tabstop = 4;
-      softtabstop = 4;
-      shiftwidth = 4;
-      expandtab = true;
-
-      relativenumber = true;
-      number = true;
-      cursorline = true;
-      termguicolors = true;
-      showmode = false;
-   
-      clipboard = "unnamedplus";
-      mouse = "";
-
-      swapfile = false;
-      undodir = "os.getenv";
-
-      incsearch = true;
-      ignorecase = true;
-      smartcase = true;
-    };
-
-    keymaps = [
-      #Telescope
-      {
-        key = "<leader>ff";
-        mode = "n";
-        options.desc ="Telescope Find File";
-        action = "<cmd>Telescope find_files<cr>";
-      }
-      {
-        key = "<leader>fg";
-        mode = "n";
-        options.desc ="Telescope Find Grep";
-        action = "<cmd>Telescope live_grep<cr>";
-      }
-      {
-        key = "<leader>fb";
-        mode = "n";
-        options.desc ="Telescope Find Buffers";
-        action = "<cmd>Telescope buffers<cr>";
-      }
-      {
-        key = "<leader>fo";
-        mode = "n";
-        options.desc ="Telescope Find Old files";
-        action = "<cmd>Telescope oldfiles<cr>";
-      }
-      {
-        key = "<leader>fz";
-        mode = "n";
-        options.desc ="Telescope Fuzzy Find";
-        action = "<cmd>Telescope current_buffer_fuzzy_find<cr>";
-      }
-
-      #FTerm
-      {
-        key = "<A-i>";
-        mode = "n";
-        options.desc ="Toggle FTerm";
-        action = "<cmd>lua require(\"FTerm\").toggle()<cr>";
-      }
-      {
-        key = "<A-i>";
-        mode = "t";
-        options.desc ="Toggle FTerm";
-        action = "<C-\\><C-n><CMD>lua require(\"FTerm\").toggle()<CR>";
-      }
-    ];
-
-    
-    extraPlugins = [ pkgs.vimPlugins.FTerm-nvim ];
-    # swag
-    colorschemes.catppuccin.enable = true;
-    plugins.lualine = {
-        enable = true;
-        theme = "catppuccin";
-    };
-    plugins.noice.enable = true;
-
-    plugins.telescope.enable = true;
-    plugins.telescope.extensions.fzf-native.enable = true;
-    plugins.telescope.extensions.undo.enable = true;
-    plugins.telescope.extensions.media_files.enable = true;
-
-    # Nerd stuff
-    plugins.treesitter.enable = true;
-    plugins.treesitter-refactor.enable = true;
-    plugins.treesitter-textobjects.enable = true;
-    plugins.ts-autotag.enable = true;
-    plugins.ts-context-commentstring.enable = true;
-
-    plugins.lsp.enable = true;
-    plugins.trouble.enable = true;
-    plugins.nvim-lightbulb.enable = true;
-    plugins.rust-tools.enable = true;
-    plugins.gitsigns.enable = true;
-    plugins.markdown-preview.enable = true;
-
-    # I use neovim btw - but on Discord
-    plugins.presence-nvim.enable = true;
-  };
-
-  programs.git = {
-    enable= true;
-    userName  = "Rodolphe Fouquet";
-    userEmail = "rodolphe@unicowd.com";
-  };
-
-  programs.kitty = {
-    enable = true;
-    theme = "Catppuccin-Mocha";
-  };
-
-  programs.starship = {
-    enable = true;
-    # Configuration written to ~/.config/starship.toml
-    settings = {
-      # add_newline = false;
-
-      character = {
-        success_symbol = "[➜](bold green)";
-        error_symbol = "[➜](bold red)";
-      };
-
-      # package.disabled = true;
-    };
-  };
-
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/vachicorne/etc/profile.d/hm-session-vars.sh
+  #
   home.sessionVariables = {
-    EDITOR = "nvim";
-    BROWSER = "google-chrome-stable";
-    TERMINAL = "kitty";
-    TERM = "kitty";
-    VISUAL = "nvim"; 
+    # EDITOR = "emacs";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  programs.fish.enable = true;
 }
