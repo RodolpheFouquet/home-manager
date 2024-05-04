824904a36f9dc2628c5ca850db85a0f45ed4abbf
Rodolphe Fouquet <rodolphe@unicowd.com>
2024-05-04 03:20:56 +0100

    add zellij config

--- home.nix
+++ home.nix
@@ -64,12 +64,16 @@
       teamspeak5_client
       bat
       plank
+      inotify-tools
+      protonvpn-cli
 
       rustup
       nodejs
       clang
       zig
       gleam
+      erlang
+      elixir
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
   ];
 
@@ -77,34 +81,9 @@
   # plain files is through 'home.file'.
   home.file = {
     ".config/wezterm/wezterm.lua".source = config/wezterm/wezterm.lua;
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    ".config/zellij/config.kdl".source = config/zellij/config.kdl;
+ };
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/vachicorne/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
